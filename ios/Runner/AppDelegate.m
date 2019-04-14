3355ac94c8d704f0c397d0b183d9367101e86e36
apple <faisalkc4u@gmail.com>
2019-04-14 11:33:11 +0400

    fixed google maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,15 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+// Add the GoogleMaps import.
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Add the following line with your API key.
+    [GMSServices provideAPIKey:@"AIzaSyDQzNlw1yKca5RCDGwfJOBAmrMwMyUMdjo"];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
